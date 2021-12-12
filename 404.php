da70ab670877c75fa460d72ab1d6370b6cc3948a
AdamMaichrzik <adam.maichrzik@wp.pl>
2021-12-12 23:15:31 +0100

    404 error website redirection

---
+++ 404.php
@@ -0,0 +1,66 @@
+<html lang="pl">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PickBox - login page</title>
+    <!-- Bootstrap CDN-->
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Gayathri:wght@400;700&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.png" />
+  </head>
+  <body>
+    <nav class="navbar sticky-top" style="background-color: gold; padding: 5px 8px">
+      <div class="col-xs-2">
+        <a href="index.php">
+          <img src="favicon.png" alt="" />
+        </a>
+      </div>
+      <div class="col-xs-8">
+        <?php
+            require_once "getMenu.php"; 
+        ?>
+        <a href="contact-page.php" class="navbar-items">Contact</a>
+      </div>
+      <div class="col-xs-2">
+        <a href="login-page.php">Login</a>
+      </div>
+    </nav>
+    <div class="container-fluid">
+      <div class="row text-center">
+          <div class="col">
+            <h3 style="color: red">Error 404 - page is not existing</h3>
+          </div>
+      </div>
+    </div>
+    <footer class="fixed-bottom">
+      <div class="footer-copyright text-center p-1">
+        © 2021 Copyright
+        <a href="index.php">PickBox</a>
+      </div>
+    </footer>
+    <script
+      src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+      integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+      integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+      integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
