7fc5d88dbb47e63c68984450a45685d57145b590
AdamMaichrzik <adam.maichrzik@wp.pl>
2021-12-27 16:57:32 +0100

    Display menu correctly and editing menu working

--- getMenu.php
+++ getMenu.php
@@ -1,5 +1,5 @@
 <?php 
-require_once "connect.php";
+require "connect.php";
 //create connection
 $connect= new mysqli($host, $db_user, $db_password, $db_name);
 
@@ -9,7 +9,7 @@ if(mysqli_connect_errno($connect))
    echo 'Failed to connect to database: '.mysqli_connect_error();
 }
 
-$result=mysqli_query($connect,"select * from dragdrop");
+$result=mysqli_query($connect,"SELECT * FROM `dragdrop` ORDER BY `dragdrop`.`listorder` ASC");
 
 
 while($row=mysqli_fetch_array($result))
--- menu.php
+++ menu.php
@@ -1,20 +1,21 @@
 <?php
+
 echo '
     <nav class="navbar sticky-top" style="background-color: gold; padding-top: 1%; padding-bottom: 1%">
       <div class="col-xs-2">
         <a href="index.php">
           <img src="favicon.png" alt="" />
         </a>
+        <button class="openbtn" onclick="openNav()">☰ Panel</button>
       </div>
-      <div class="col-xs-8">'
-
-      ,require_once "getMenu.php";
-
+      <div class="col-xs-8">
+      '
+      ;include 'getMenu.php';
+      
       echo '
       </div>
       <div class="col-xs-2">
         <a href="Login-Page">Login</a>
       </div>
-    </nav>
-    '
+    </nav>'
 ?>
