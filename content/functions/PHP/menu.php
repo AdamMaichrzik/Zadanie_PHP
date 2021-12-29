6c0a649e9eb5dae86e21623c57d2b4fa8a927098
AdamMaichrzik <adam.maichrzik@wp.pl>
2021-12-29 16:14:34 +0100

    Company logo menu path

--- content/functions/PHP/menu.php
+++ content/functions/PHP/menu.php
@@ -3,8 +3,8 @@
 echo '
     <nav class="navbar sticky-top" style="background-color: gold; padding-top: 1%; padding-bottom: 1%">
       <div class="col-xs-2">
-        <a href="index.php">
-          <img src="favicon.png" alt="" />
+        <a href="/index.php">
+          <img src="../../img/favicon.png" alt="Company logo" />
         </a>'
         ;if (strpos($_SERVER['REQUEST_URI'], "Admin") !== false){
           echo '<button class="openbtn" onclick="openNav()">☰ Panel</button>';
