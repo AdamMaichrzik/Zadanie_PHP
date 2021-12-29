a1007b9173bd815c54332f5d7da94dfdf71184a7
AdamMaichrzik <adam.maichrzik@wp.pl>
2021-12-29 16:14:53 +0100

    Admin left navbar show/hide on click

--- admin/functions/JS/adminPanelNavbar.js
+++ admin/functions/JS/adminPanelNavbar.js
@@ -1,10 +1,21 @@
-  function openNav() {
-    document.getElementById("mySidebar").style.width = "189px";
-    document.getElementById("main").style.marginLeft = "189px";
-  }
+var opened = false;
 
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+function openNav(){
+  if(opened == false){
+    opened = true;
+    openNavv();
+  } if(opened == true){
+    opened = false;
+    closeNav();
   }
+}
 
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+function openNavv() {
+  document.getElementById("mySidebar").style.width = "189px";
+  document.getElementById("main").style.marginLeft = "189px";
+}
--- admin/functions/PHP/makeLeftNavbar.php
+++ admin/functions/PHP/makeLeftNavbar.php
@@ -1,7 +1,6 @@
 <?php
     echo'
     <div id="mySidebar" class="sidebar">
-        <a href="javascript:void(0)" class="closebtn" style="color: tomato" onclick="closeNav()">X</a>
         <div class="admin-menu-tile"><a href="Admin-Panel">Admin Panel</a></div>
         <div class="admin-menu-tile"><a href="Admin-Panel-Company-Info">Company info</a></div>
         <div class="admin-menu-tile"><a href="Admin-Panel-Make-Website">Make Website</a></div>
