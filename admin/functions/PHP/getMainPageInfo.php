020cd4735b5b54b486a9eef2b7cee7a19d1dfb1d
AdamMaichrzik <adam.maichrzik@wp.pl>
2022-05-20 08:04:11 +0200

    Deletes code cousing problem

--- admin/functions/PHP/getMainPageInfo.php
+++ admin/functions/PHP/getMainPageInfo.php
@@ -3,12 +3,6 @@ require_once "connect.php";
 //create connection
 $connect= new mysqli($host, $db_user, $db_password, $db_name);
 
-//check connection
-if(mysqli_connect_errno($connect))
-{
-   echo 'Failed to connect to database: '.mysqli_connect_error();
-}
-
 $result=mysqli_query($connect,"select * from mainPage");
 
 
